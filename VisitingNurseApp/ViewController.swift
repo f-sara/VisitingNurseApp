a879aa088906bc88a62f5e4072990ab2891c4d9c
f-sara <jyujyusa.68@gmail.com>
2024-06-26 12:00:11 +0900

    Initial Commit

---
+++ VisitingNurseApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VisitingNurseApp
+//
+//  Created by 藤井紗良 on 2024/06/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
