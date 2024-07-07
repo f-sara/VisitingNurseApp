2f4f647429eea9c5c377ca7f91b34f66fb8c3c74
f-sara <jyujyusa.68@gmail.com>
2024-07-07 15:04:12 +0900

    test: 仮のテストコードを作成 #3

--- VisitingNurseAppTests/LoginTests.swift
+++ VisitingNurseAppTests/LoginTests.swift
@@ -8,3 +8,28 @@
 import XCTest
 @testable import VisitingNurseApp
 
+final class LoginTests: XCTestCase {
+
+    private let loginModel = LoginModel()
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+
+    private func testSuccessAuthentication() {
+        let auth = loginModel.authentication(auth: true)
+        XCTAssertEqual(true, auth)
+    }
+
+    private func testFailAuthentication() {
+        let auth = loginModel.authentication(auth: false)
+        XCTAssertEqual(false, auth)
+    }
+
+
+}
