406fe679467cb5eed79f6f6c380a6a2eb1287e15
f-sara <jyujyusa.68@gmail.com>
2024-09-04 12:59:40 +0900

    feat: ログイン画面の表示を切り替える処理を追加 #6

--- VisitingNurseApp/SceneDelegate.swift
+++ VisitingNurseApp/SceneDelegate.swift
@@ -11,12 +11,20 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    let loginFlag = false
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+
+        if !loginFlag {
+            let storyboard = UIStoryboard(name: R.storyboard.mainCalendar.name, bundle: nil)
+            let mainViewController = storyboard.instantiateViewController(withIdentifier: R.storyboard.mainCalendar.name)
+            window?.rootViewController = mainViewController
+        }
+
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
