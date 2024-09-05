cdf6c8ab92a344dae2f49771e34306a67143737e
f-sara <jyujyusa.68@gmail.com>
2024-09-05 11:26:20 +0900

    add: 各画面の仮ファイルを追加 #6

---
+++ VisitingNurseApp/View/ViewController/PatientListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PatientListViewController.swift
+//  VisitingNurseApp
+//
+//  Created by 藤井紗良 on 2024/09/04.
+//
+
+import UIKit
+
+class PatientListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
