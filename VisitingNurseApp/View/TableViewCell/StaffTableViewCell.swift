b04ebfa5975edb1e0f195a204568c2dad32f94ce
f-sara <jyujyusa.68@gmail.com>
2024-09-10 11:56:53 +0900

    add: StaffListのtableViewCellを追加 #5

---
+++ VisitingNurseApp/View/TableViewCell/StaffTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  StaffTableViewCell.swift
+//  VisitingNurseApp
+//
+//  Created by 藤井紗良 on 2024/09/10.
+//
+
+import UIKit
+
+class StaffTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
